b6a360aa2d4909660e2e6ad192c14efe82492546
rinkykumari89 <02rinky.dev@gmail.com>
2021-12-06 18:19:35 +0530

    retrieving from localStorage

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
-console.log('started js');
-
-
-var correctAnswer = document.getElementById('correctAnswerId').value;
-console.log(correctAnswer);
-
-//function to store scores
+//Function to store scores in LocalStorage
 function store(){
     var correctAnswer = document.getElementById('correctAnswerId').value;
     var scoreKey = document.getElementById('scoreKeyId').value;
@@ -12,16 +6,33 @@ function store(){
     console.log(correctAnswer);
     const score = {
         correctAnswer: correctAnswer,
-        score: correctAnswer * 10,
+        totalScore: correctAnswer * 10,
     }
 
     window.localStorage.setItem(scoreKey, JSON.stringify(score));
 
 }
 
-//window.alert('how!');
+//Fuction to retrieve score from LocalStorage
+function retrieve(){
+
+     var retrieveKey = document.getElementById('retrieveKeyId').value;
+     var scoreRecords = JSON.parse(window.localStorage.getItem(retrieveKey));
+     var para = document.createElement("p");                     //created paragraph element
+     var info = document.createTextNode("Your score is : \t"+ scoreRecords.totalScore);
+     para.appendChild(info);   
+     var element = document.getElementById('retrieveClass');  
+     element.appendChild(para); 
+
+}
+
+//function to retrieve scores
+
+/*-------- working only on refresh page -----------------------------
 
 window.onload = function(){
-    document.getElementById('scoreFormId').addEventListener("onsubmit", function(){ alert("Hello World!"); }); 
+    document.getElementById('scoreFormId').onclick = store(); 
 }
+
+--------------------------------------------------------------*/
 
