e57460893a053911c9685ab394ec4373dd004a1c
rinkykumari89 <02rinky.dev@gmail.com>
2021-12-07 17:33:25 +0530

    updated with remove and clear function

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
-console.log('started js');
-
-
-var correctAnswer = document.getElementById('correctAnswerId').value;
-console.log(correctAnswer);
-
-//function to store scores
+//---Function to store scores in LocalStorage-------------------------------
 function store(){
     var correctAnswer = document.getElementById('correctAnswerId').value;
     var scoreKey = document.getElementById('scoreKeyId').value;
@@ -12,16 +6,55 @@ function store(){
     console.log(correctAnswer);
     const score = {
         correctAnswer: correctAnswer,
-        score: correctAnswer * 10,
+        totalScore: correctAnswer * 10,
     }
 
     window.localStorage.setItem(scoreKey, JSON.stringify(score));
 
 }
 
-//window.alert('how!');
+//---Fuction to retrieve score from LocalStorage-------------------------------
+function retrieve(){
 
-window.onload = function(){
-    document.getElementById('scoreFormId').addEventListener("onsubmit", function(){ alert("Hello World!"); }); 
+     var retrieveKey = document.getElementById('retrieveKeyId').value;
+     var scoreRecords = JSON.parse(window.localStorage.getItem(retrieveKey));
+
+    //created paragraph element
+     var para = document.createElement("p");                     
+     var info = document.createTextNode("Your score is : \t"+ scoreRecords.totalScore);
+     para.appendChild(info); 
+
+     var element = document.getElementById('retrieveId');  
+     element.appendChild(para); 
+
 }
+
+//-----Function to remove score from LocalStorage-------------------------------
+function remove(){
+
+    var removeKey = document.getElementById('removeKeyId').value;
+    localStorage.removeItem(removeKey);
+
+    //Created Paragraph element
+    var para = document.createElement("p");
+    var info = document.createTextNode("Score is Successfully deleted!");
+    para.appendChild(info);
+
+    var element = document.getElementById('removeId');
+    element.appendChild(para);    
+}
+
+//-----Function to clear LocalStorage------------------------------------------------
+function clearLocal(){
+    localStorage.clear();
+
+    //created Paragraph element
+    var para = document.createElement("p");
+    var info = document.createTextNode("Successfully cleared!");
+    para.appendChild(info);
+
+    var element = document.getElementById('clearId');
+    element.appendChild(para);
+}
+
 
